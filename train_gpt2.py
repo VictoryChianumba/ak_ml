3274f7b011b2adbdc2fd17b1edcbef5e32e3db3a
Victory Chianumba <chianumbav@gmail.com>
2025-04-10 17:52:14 +0100

    idk why

--- train_gpt2.py
+++ train_gpt2.py
@@ -7,71 +7,77 @@ import math
 
 # -------------------------------------------------
 class CausalSelfAttention(nn.Module):
-
+    
     def __init__(self, config):
         super().__init__()
-        assert config.n_embd % config.n_head == 0
+        assert config.n_embd % config.n_head == 0  
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
-        # output projection
+        # output projection 
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
-        self.c_proj.NANOGPT_SCALE_INIT = 1
-        # regularization
+        # self regularization 
         self.n_head = config.n_head
         self.n_embd = config.n_embd
-
+        # Not really a 'bias', more of a mask, but following the OpenAI/HF naming 
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+        
     def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
-        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        B, T, C = x.size() # Batch sizer, sequence length, embedding dimensionality 
+        # Calculate query, key and value for all heads in the batch and move head forward to be the batch
+        # nh is 'Number of Heads', hs is 'Head Size' and C (number of channels) = nh * hs
+        # e.g in GPT-2(124M), n_heads = 12, hs = 64, so nh * hs = 768 channels in the transformer
         qkv = self.c_attn(x)
         q, k, v = qkv.split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-        # output projection
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs) 
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs) 
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, nh, T, hs) 
+        # attention (materializes the large (T, T) matrix for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim = 1)
+        y = att @ v 
+        y = y.transpose(1, 2).contiguous().view(B,T,C) # assemble all head outputs side by side 
+        # output projection 
         y = self.c_proj(y)
-        return y
+        return y 
 
 class MLP(nn.Module):
-
+    
     def __init__(self, config):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
-        self.gelu    = nn.GELU(approximate='tanh')
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
-        self.c_proj.NANOGPT_SCALE_INIT = 1
-
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4*config.n_embd, config.n_embd)
+        
     def forward(self, x):
-        x = self.c_fc(x)
-        x = self.gelu(x)
-        x = self.c_proj(x)
+        x = self.c_fc
+        x = self.gelu
+        x = self.c_proj
         return x
+        
 
 class Block(nn.Module):
-
+    
     def __init__(self, config):
-        super().__init__()
-        self.ln_1 = nn.LayerNorm(config.n_embd)
+        super().__init()
+        self.ln_1 = nn.LayerNorm(config.vocab_size)
         self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
-
+        
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
-
+        
 @dataclass
 class GPTConfig:
-    block_size: int = 1024 # max sequence length
-    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
-    n_layer: int = 12 # number of layers
-    n_head: int = 12 # number of heads
-    n_embd: int = 768
+    block_size = 1024
+    vocab_size = 50257
+    n_layer = 12
+    n_head = 12
+    n_embd = 768
     
 class GPT(nn.Module):
     def __init__(self, config):
@@ -162,7 +168,7 @@ class GPT(nn.Module):
 
 model = GPT.from_pretrained('gpt2')
 # model.eval()    
-print('yay i didnt crash yups')
+print('yay i didnt crash')
 
 # device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 # model.to(device)
